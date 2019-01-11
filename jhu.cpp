1786fc6544ae2fc2afef3d2605446a4a35647ba8
FelixDoot <46586295+FelixDoot@users.noreply.github.com>
2019-01-10 21:16:32 -0700

    Add files via upload
    Ah Yes, Pornhub.com

---
+++ jhu.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+int main(void)
+{
+std::string name = " Lemon Lasagna";
+    std::cout<<"Hello" << name << std::endl;
+    return (0);
+}
+
+int (
+{
+	std::string name = "I Guess They Never Miss Huh";
+	std::cout<<"Hit Or Miss " << name << std::endl;
+	return (0);
+}
+
+#include <string>
+#include <iostream>
+using namespace std;
+#include "console.h"
+int main() {
+string small, large;
+small = "I am short";
+large = "I, friend, am a long and elaborate string indeed";
+cout << "The short string is " << small.length()
+ << " characters." << endl;
+cout << The long string is " << large.length()
+ << " characters." << endl;
+return 0;
+}
